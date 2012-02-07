dca69c86c4daee31a45fa22283bbd4ebe848fb34
Joel Griffith <joel@joel-griffith.com>
2012-02-07 16:03:38 -0500

    add kalman filter

---
+++ project1/kalmanfilter.cpp
@@ -0,0 +1,23 @@
+#include "kalmanfilter.h"
+
+KalmanFilter::KalmanFilter(Pose* initialPose) {
+	_curPose = initialPose;
+	// TODO: does _kf need to be explicitly initialized?
+	float initialPoseArr[3];
+	initialPose->toArray(initialPoseArr);
+	float velocity[3] = {0, 0, 0};
+	initKalmanFilter(&_kf, initialPoseArr, velocity, 1);
+}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::filter(Pose* nsPose, Pose* wePose) {
+	float nsPoseArr[3];
+	float wePoseArr[3];
+	nsPose->toArray(nsPoseArr);
+	wePose->toArray(wePoseArr);
+	rovioKalmanFilter(&_kf, nsPoseArr, wePoseArr, _track);
+	_curPose.setX(_track[0]);
+	_curPose.setY(_track[1]);
+	_curPose.setTheta(_track[2]);
+}
---
+++ project1/kalmanfilter.h
@@ -0,0 +1,19 @@
+#ifndef CS1567_KALMANFILTER_H
+#define CS1567_KALMANFILTER_H
+
+#include "kalmanFilterDef.h"
+#include "pose.h"
+
+class KalmanFilter {
+public:
+	KalmanFilter(Pose* initialPose);
+	~KalmanFilter();
+	void filter(Pose* nsPose, Pose* wePose);
+private:
+	kalmanFilter _kf;
+	float _track[9];
+
+	Pose* _curPose;
+};
+
+#endif
