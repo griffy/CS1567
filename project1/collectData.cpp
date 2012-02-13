6fe696398c6e6706093b81f79325a894efd82fe3
Thomas Nason <tdn10@pitt.edu>
2012-02-13 16:44:47 -0500

    Tweaked NS constants, entire room should now be (mostly) accurately converted into a global coordinate system. Modified data collection file to wait and collect final position after moving.

--- project1/collectData.cpp
+++ project1/collectData.cpp
@@ -70,18 +70,27 @@ int main(int argc, char *argv[]) {
         printf("prefilling data\n");
         robot->update();
         //write to global position file
-        fprintf(outfileglobal,"%f,%f,%f\n",robot->_getNSTransX(),robot->_getNSTransY(),robot->_getNSTransTheta());
+        fprintf(outfileglobal,"%d %f,%f,%f\n",robot->_robotInterface->RoomID(),robot->_getNSTransX(),robot->_getNSTransY(),robot->_getNSTransTheta());
         //write to global position file
         fprintf(outfilewe,"%d,%d,%d\n",robot->_robotInterface->getWheelEncoder(RI_WHEEL_LEFT),robot->_robotInterface->getWheelEncoder(RI_WHEEL_RIGHT),robot->_robotInterface->getWheelEncoder(RI_WHEEL_REAR));
     }
 
-    for (int i = 0; i < 30; i++) {
-        robot->moveForward(5);
+    for (int i = 0; i < 20; i++) {
+        robot->moveForward(10);
         robot->update();
         //write to global position file
-        fprintf(outfileglobal,"%f,%f,%f\n",robot->_getNSTransX(),robot->_getNSTransY(),robot->_getNSTransTheta());
+        fprintf(outfileglobal,"%d %f,%f,%f\n",robot->_robotInterface->RoomID(),robot->_getNSTransX(),robot->_getNSTransY(),robot->_getNSTransTheta());
         fprintf(outfilewe,"%d,%d,%d\n",robot->_robotInterface->getWheelEncoder(RI_WHEEL_LEFT),robot->_robotInterface->getWheelEncoder(RI_WHEEL_RIGHT),robot->_robotInterface->getWheelEncoder(RI_WHEEL_REAR));
     }
+
+
+    for (int i = 0; i < 10; i++) {
+        robot->update();
+        //write to global position file
+        fprintf(outfileglobal,"%d %f,%f,%f\n",robot->_robotInterface->RoomID(),robot->_getNSTransX(),robot->_getNSTransY(),robot->_getNSTransTheta());
+        fprintf(outfilewe,"%d,%d,%d\n",robot->_robotInterface->getWheelEncoder(RI_WHEEL_LEFT),robot->_robotInterface->getWheelEncoder(RI_WHEEL_RIGHT),robot->_robotInterface->getWheelEncoder(RI_WHEEL_REAR));
+    }
+
 
     fflush(outfileglobal);
     fflush(outfilewe);
