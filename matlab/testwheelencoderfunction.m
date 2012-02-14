f1db7b637e81726dae368d6fb77825c282da648f
Joel Griffith <joel@joel-griffith.com>
2012-02-14 16:20:40 -0500

    update wheel encoder matlab function

--- matlab/testwheelencoderfunction.m
+++ matlab/testwheelencoderfunction.m
@@ -1,5 +1,5 @@
-data=turnLeftGetWheelencoderLeft_we_global;
-nsData=turnLeftGetWheelencoderLeft_ns_global(:,3)
+data=turnLeftWE_we_global;
+nsData=turnLeftWE_ns_global(:,4)
 
 weold=0;
 leftDeltaX = data(:,1)
@@ -15,29 +15,32 @@ sumY=sum(rightDeltaX)
 sumTheta=sum(rearDeltaX)
 
 circumferenceOfWheel=23.9; %in centimeters 3 inch wheel
-radius=29/2.0;
+
+
+radius=29/(2); %diameter of robot
+circumferencOfRobot=radius*2*pi;
 
 ticksperRev=103;
 
 ticksPerCm=ticksperRev/(circumferenceOfWheel)
-ticksPerCm=4;
+%ticksPerCm=4;
 
 for i = 1:length(data)
 
-w1=leftDeltaX(i)/ticksPerCm
-w2=rightDeltaX(i)/ticksPerCm
-w3=rearDeltaX(i)/ticksPerCm
+w1=leftDeltaX(i)/ticksPerCm;
+w2=rightDeltaX(i)/ticksPerCm;
+w3=rearDeltaX(i)/ticksPerCm;
 
-lengthArc=-(w1-w2+w3)
-thetaNew=(lengthArc/(pi*radius));
-thetaNew=((-w3))/(radius)
+lengthArc=-(w1-w2+w3)/3
+thetaNew=(lengthArc/(radius));
+thetaNew=((w3))/(radius)
 
-dy=((w2*sind(30) + w1*sind(-30))/2)*sind(weold)
-dx=((w2*cosd(30) + w1*cosd(-30))/2)*cosd(weold)
+dy=((w2*sind(30) + w1*sind(-30))/2)*sind(weold);
+dx=((w2*cosd(30) + w1*cosd(-30))/2)*cosd(weold);
 
 
-currX=currX+dx
-currY=currY+dy
+currX=currX+dx;
+currY=currY+dy;
 weold=weold+thetaNew
 
 end
@@ -45,6 +48,6 @@ end
 
 changeinNS=nsData(length(nsData))-nsData(1)
 
-ticksPerCm
+ticksPerCm;
 
 differenceFromNS=radtodeg(changeinNS-weold)
