9b230e4d7b1974b20b7f845bb1c8a123762f5bcb
Joel Griffith <joel@joel-griffith.com>
2012-03-20 14:56:39 -0400

    add logging to north star to see what's wrong

--- project2/north_star.cpp
+++ project2/north_star.cpp
@@ -1,5 +1,6 @@
 #include "north_star.h"
 #include "constants.h"
+#include "logger.h"
 
 NorthStar::NorthStar(RobotInterface *robotInterface)
 : PositionSensor(robotInterface) {
@@ -20,7 +21,15 @@ void NorthStar::updatePose(int room) {
 	float y = _getFilteredY();
 	float theta = _getFilteredTheta();
 
+	LOG.write(LOG_LOW, "ns estimates", 
+			  "filtered x: %f, filtered y: %f, filtered theta: %f",
+			  x, y, theta);
+
 	Pose *estimate = new Pose(x, y, theta);
+	LOG.write(LOG_LOW, "ns estimates", 
+			  "pose start x: %f, pose start y: %f, pose start theta: %f",
+			  estimate->getX(), estimate->getY(), estimate->getTheta());
+
 	if (room == ROOM_2) {
 		// Apply specific linear transformation to Room 2, 
 		// to correct for theta skew
@@ -31,18 +40,34 @@ void NorthStar::updatePose(int room) {
 	else {
 		estimate->rotate(NS_ROOM_ROTATION[room]);
 	}
+	LOG.write(LOG_LOW, "ns estimates", 
+		  	  "room %d, pose rotate x: %f, pose rotate y: %f, pose rotate theta: %f",
+		      room, estimate->getX(), estimate->getY(), estimate->getTheta());
+
 	float sx = NS_ROOM_SCALE[room][0];
 	float sy = NS_ROOM_SCALE[room][1];
 	estimate->scale(sx, sy);
+	LOG.write(LOG_LOW, "ns estimates", 
+		  	  "room %d, pose scale x: %f, pose scale y: %f, pose scale theta: %f",
+		      room, estimate->getX(), estimate->getY(), estimate->getTheta());
+
 	float tx = COL_OFFSET[0] + NS_ROOM_ORIGINS_FROM_COL[room][0];
 	float ty = COL_OFFSET[1] + NS_ROOM_ORIGINS_FROM_COL[room][1];
 	estimate->translate(tx, ty);
+
+	LOG.write(LOG_LOW, "ns estimates", 
+		  	  "room %d, pose translate x: %f, pose translate y: %f, pose translate theta: %f",
+		      room, estimate->getX(), estimate->getY(), estimate->getTheta());
 
 	_adjustTotalTheta(theta);
 
 	_pose->setX(estimate->getX());
 	_pose->setY(estimate->getY());
 	_pose->setTheta(estimate->getTheta());
+
+	LOG.write(LOG_LOW, "ns estimates", 
+		  	  "new pose x: %f, new pose y: %f, new pose theta: %f",
+		      _pose->getX(), _pose->getY(), _pose->getTheta());
 
 	delete estimate;
 }
